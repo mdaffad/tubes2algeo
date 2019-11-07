31f7ea7231cf73b57d0f1496a525c12b9ad3d51e
aufaf29 <47714476+aufaf29@users.noreply.github.com>
2019-11-08 05:35:15 +0700

    Update newui.py

--- newui.py
+++ newui.py
@@ -5,7 +5,9 @@ from PIL import ImageTk, Image
 import os
 from newrunable import run
 import numpy as np
+
 objPic = ' '
+filepath = ' '
 window = Tk()
 window.title("Face Recognition 3.14")
 window.geometry("1000x400")
@@ -13,18 +15,21 @@ count = 0
 L = np.array([])
 option = True
 entry = 0
+
 def RunEuclidean():
     global entry
     global objPic
     global count
+    global filepath
     count = 0
-    ShowImageList(True,objPic,entry)
+    ShowImageList(True,objPic,entry,filepath)
 def RunCosine():
     global entry
     global objPic
     global count
+    global filepath
     count = 0
-    ShowImageList(False,objPic,entry)
+    ShowImageList(False,objPic,entry,filepath)
 
 def ShowImageListLanjutan():
     global L
@@ -36,24 +41,27 @@ def ShowImageListLanjutan():
     label2.image = image2X
     label2.place(x=320, y=55)
 
-def ShowImageList(option, address, top):
+def ShowImageList(option, address, top, fpath):
     global L
-    L = np.copy(run(option, address, top))
+    L = np.copy(run(option, address, top, fpath))
     image2 = Image.open(L[0])
     image2 = image2.resize((270, 270), Image.ANTIALIAS)
     image2X = ImageTk.PhotoImage(image2)
     label2 = Label(window, image=image2X)
     label2.image = image2X
     label2.place(x=320, y=55)
-    
-def Next():
-    global count
-    count +=1
-    ShowImageListLanjutan(count)
 
 def Directory():
     filename = filedialog.askopenfilename(title='Choose Picture')
     return filename
+
+def DirectoryFolder():
+    filename = filedialog.askdirectory(title='Choose Directory')
+    return filename
+
+def ChooseFolder():
+    global filepath
+    filepath = DirectoryFolder()
 
 def ImageProccess():
     global objPic
@@ -78,19 +86,25 @@ def Next():
         ShowImageListLanjutan()
 
 
-# Entri Top Number
+#Set Latar
+background = Image.open('/home/aufa/Desktop/Fix/Layout.jpg')
+imageX = ImageTk.PhotoImage(background)
+labelX = Label(window, image=imageX)
+labelX.image = imageX
+labelX.place(x=0, y=0)
 
-
-tk.Label(window, text="Input Top Number").place(x = 800,  y = 130)
+#Set Entry
+tk.Label(window, text="Input Top Number").place(x=747 ,  y=130)
 e1 = tk.Entry(window)
-e1.place(x= 800, y = 150)
+e1.place(x= 720, y = 150)
+tk.Button(window, text='Set', command = getEntry).place(x= 900,  y= 143)
 
-tk.Button(window, text='Show', command = getEntry).place(x= 700,  y= 150)
+#Set Button
+buttonDir = Button(window, text='Choose Folder', command=ChooseFolder).place(x= 750, y= 310)
+buttonFile = Button(window, text='Choose Image', command=ImageProccess).place(x=760, y=215)
+buttonEuclidean = Button(window, text='Euclidean', command=RunEuclidean).place(x= 730,  y=250 )
+buttonCos = Button(window, text='Cosine', command=RunCosine).place(x= 830,  y= 250)
+buttonNext = Button(window, text = 'Next Result', command = Next).place(x= 764, y= 345)
 
-buttonFile = Button(window, text='Choose Image', command=ImageProccess).place(x=800, y=300)
-buttonEuclidean = Button(window, text='Euclidean', command=RunEuclidean).place(x= 800,  y=200 )
-buttonCos = Button(window, text='Cosine', command=RunCosine).place(x= 800,  y= 250)
-buttonNext = Button(window, text = 'Next Result', command = Next).place(x = 600, y = 350)
 
-# window.iconbitmap(r'/home/aufa/Desktop/Fix/panah.ico')
 window.mainloop()
